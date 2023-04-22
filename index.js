9ae9e727bc66ea4a0fb4b85335978cd9ef61cdfe
Jocelyn <jocelynvargas123@yahoo.com>
2023-04-22 12:50:23 -0700

    fetch lab

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json))
   // To pass the tests, don't forget to return your fetch!
   
 }
